de0487d2f48097c29a91116d5214bc4c0d1d51fd
Ng Zhan Qing <zhanqing152366@live.com>
2023-09-27 03:16:36 +0800

    updated all

--- ItpApp.py
+++ ItpApp.py
@@ -32,8 +32,43 @@ def home():
 
 @app.route("/fetchdata", methods=['GET', 'POST'])
 def fetch_student_data():
-  
-    
+        # Define the SQL query to fetch data from the "assignment" table based on student_id
+        student_id = 123456  # Replace with the actual student_id you want to fetch
+        sql_query = """
+            SELECT 
+                cohort, intern_period, status, remark, 
+                student_name, student_id, student_NRIC, 
+                student_gender, student_email, mobile_number, 
+                supervior_name, supervisor_email, 
+            FROM assignment
+            WHERE student_id = %s
+        """
+
+        cursor = db_conn.cursor()
+        cursor.execute(sql_query, (student_id,))
+        student_data = cursor.fetchone()
+
+        if student_data:
+            # Convert the fetched data into a dictionary
+            student_dict = {
+                "cohort": student_data[0],
+                "intern_period": student_data[1],
+                "status": student_data[2],
+                "remark": student_data[3],
+                "student_name": student_data[4],
+                "student_id": student_data[5],
+                "student_NRIC": student_data[6],
+                "student_gender": student_data[7],
+                "student_email": student_data[8],
+                "mobile_number": student_data[9],
+                "supervior_name": student_data[10],
+                "supervisor_email": student_data[11],
+            }
+
+            return render_template('student.html', student_data=student_dict)
+        else:
+            return "Student not found"
+
 
 @app.route("/addcompany", methods=['POST'])
 def AddCompany():
