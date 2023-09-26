be63d015f1fc15d0cf77c2875db3cc5353dfabac
Ng Zhan Qing <zhanqing152366@live.com>
2023-09-27 05:02:48 +0800

    Revert "Update ItpApp.py"
    This reverts commit 6c034147a0d92145e95aeae9a3eb54fea874643f.

--- ItpApp.py
+++ ItpApp.py
@@ -56,11 +56,10 @@ def fetch_student_data():
                 "student_id": student_data[5],
                 "student_NRIC": student_data[6],
                 "student_gender": student_data[7],
-                "student_programme": student_data[8],
-                "student_email": student_data[9],
-                "mobile_number": student_data[10],
-                "supervisor_name": student_data[11],
-                "supervisor_email": student_data[12]
+                "student_email": student_data[8],
+                "mobile_number": student_data[9],
+                "supervior_name": student_data[10],
+                "supervisor_email": student_data[11],
             }
 
             return jsonify(student_dict)
