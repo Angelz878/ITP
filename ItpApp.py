dd42021b5452b9a5474b2d23aeadbda21fc1437d
Ng Zhan Qing <zhanqing152366@live.com>
2023-09-27 22:31:25 +0800

    Update ItpApp.py

--- ItpApp.py
+++ ItpApp.py
@@ -265,8 +265,7 @@ def AddCandidate():
     remark = request.form['remark']    
     student_address = request.form['student_address']
     mobile_number = request.form['mobile_number']
-    status = "pending"
-
+    
     # SQL insert statement for inserting all data into a single table
     insert_sql = "INSERT INTO assignment (level, cohort, student_programme, intern_period, student_group, student_id, student_email, cgpa, supervisor_name, supervisor_email, student_name, student_NRIC, student_gender, remark, student_address, mobile_number, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 'pending')"
 
