10e81cf3094af6052014fe925cbb19f85e2cf90c
Yean1277 <looyeanter021115@gmail.com>
2023-09-27 18:09:09 +0800

    update login

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
 from pymysql import connections
+from config import *
 import re
   
 app = Flask(__name__)
